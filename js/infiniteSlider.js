d0f8e7c05367a40c1e908ec011e0886b837c8491
David Fernandes <160598395+dav1-fer@users.noreply.github.com>
2025-07-01 17:00:15 +0100

    Add files via upload

--- js/infiniteSlider.js
+++ js/infiniteSlider.js
@@ -1,11 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const sliders = document.querySelectorAll(".testimonial-slider");
-
-  sliders.forEach(slider => {
-    const cards = Array.from(slider.children);
-    cards.forEach(card => {
-      const clone = card.cloneNode(true);
-      slider.appendChild(clone);
+  requestAnimationFrame(() => {
+    const sliders = document.querySelectorAll(".testimonial-slider");
+    sliders.forEach(slider => {
+      const cards = Array.from(slider.children);
+      cards.forEach(card => {
+        const clone = card.cloneNode(true);
+        slider.appendChild(clone);
+      });
     });
   });
 });
