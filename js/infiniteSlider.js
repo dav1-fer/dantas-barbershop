d2242509bc8d2c9eb243a0e96476e491b00a5854
David Fernandes <160598395+dav1-fer@users.noreply.github.com>
2025-07-03 19:36:54 +0100

    Infinite Testimonial Slider Updated

--- js/infiniteSlider.js
+++ js/infiniteSlider.js
@@ -1,12 +1,21 @@
-document.addEventListener("DOMContentLoaded", () => {
-  requestAnimationFrame(() => {
-    const sliders = document.querySelectorAll(".testimonial-slider");
-    sliders.forEach(slider => {
-      const cards = Array.from(slider.children);
-      cards.forEach(card => {
-        const clone = card.cloneNode(true);
-        slider.appendChild(clone);
-      });
-    });
-  });
-});
+const sliderWrapper = document.querySelectorAll(".testimonial-slider");
+
+if(!window.matchMedia("(prefers-reduced-motion: reduce").matches) {
+    addAnimation();
+}
+
+function addAnimation() {
+    sliderWrapper.forEach(slider => {
+        slider.setAttribute('data-animate', true);
+
+        const sliderContainer = slider.querySelector(".testimonial-content");
+        const sliderContent = Array.from(sliderContainer.children);
+
+
+        sliderContent.forEach(item => {
+            const cloneItem = item.cloneNode(true);
+            cloneItem.setAttribute("aria-hidden", true);
+            sliderContainer.appendChild(cloneItem);
+        })
+    })
+}
